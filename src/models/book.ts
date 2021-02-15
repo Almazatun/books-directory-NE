17b6fbb79680b3beac20ce21d42d0673e4ad47ba
Almaz <atunstyle@gmail.com>
2021-02-15 11:44:47 +0300

    add new model *Added Book model.

---
+++ src/models/book.ts
@@ -0,0 +1,48 @@
+import mongoose, {Schema, Document} from 'mongoose'
+
+export interface IBook extends Document {
+    _id: Schema.Types.ObjectId
+    title: string
+    authorName: Schema.Types.ObjectId
+    description: string
+    pageCount: number
+    publishDate: string
+    createAt: string
+    coverImageName: string
+    _doc?: object
+}
+
+const BookSchema: Schema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    publishDate: {
+        type: String,
+        required: true
+    },
+    authorName: {
+        //Referencing to Authors collections
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Author'
+    },
+    pageCount: {
+        type: Number,
+        required: true
+    },
+    createAt: {
+        type: String,
+        required: true,
+        default: Date.now()
+    },
+    coverImageName: {
+        type: String,
+        required: true
+    }
+})
+
+export default mongoose.model<IBook>('Book', BookSchema)
