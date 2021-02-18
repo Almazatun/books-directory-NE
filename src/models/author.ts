c3c5ea8abfa23c90ef86586d10da499aed186cee
Almaz <atunstyle@gmail.com>
2021-02-18 18:52:41 +0300

    :label: upload author schema module

--- src/models/author.ts
+++ src/models/author.ts
@@ -2,17 +2,26 @@ import mongoose, {Schema, Document} from 'mongoose'
 
 export interface IAuthor extends Document {
     _id: Schema.Types.ObjectId
-    name: string,
+    fistName: string
+    lastName: string
     createdAt: string
     _doc?: object
 }
 
 const AuthorSchema: Schema = new Schema({
-   name: {
+    fistName: {
        type: String,
        required: true
    },
-    createAt: String
+    lastName: {
+        type: String,
+        required: true
+    },
+    createAt: {
+        type: String,
+        required: true,
+        default: new Date().toISOString()
+    },
 })
 
 export default mongoose.model<IAuthor>('Author', AuthorSchema)
