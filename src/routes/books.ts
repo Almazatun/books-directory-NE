9ed36070d43eb9730f1d4b94598113bdfeaed509
Almaz <atunstyle@gmail.com>
2021-02-20 00:06:30 +0300

    include new route and upload file *Included (/uploaded) route.

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import controller from '../controllers/books'
+import {upload} from "../utils/multerStorage";
 
 const router = express.Router();
 
-//Get all authors
+//Get all books
 router.get("/", controller.getAllBooks)
 
-//Create new author
+//Create new book
 router.post("/new", controller.createNewBook)
+
+//Upload new book image
+router.post("/upload", upload.single('cover'), controller.uploadBookImage)
+
+//Delete uploaded image file
+router.delete("/uploaded", controller.deleteUploadedFile)
+
+//Delete book
+router.delete("/delete/:id" ,controller.deleteBook)
 
 export default router;
