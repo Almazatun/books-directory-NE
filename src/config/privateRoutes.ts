a6a8f3dbc12e8c02e61d63c8728f45df72707e86
Almaz <atunstyle@gmail.com>
2021-02-25 16:20:15 +0300

    update route file and add new config file * Added privateRoutes config file.

---
+++ src/config/privateRoutes.ts
@@ -0,0 +1,2 @@
+export const IMAGES_ROUTE_URL_GET: string = process.env.IMAGES_ROUTE_URL_GET || '/'
+export const IMAGES_ROUTE_URL_DELETE: string = process.env.IMAGES_ROUTE_URL_DELETE || "/uploaded/delete/:id"
--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import controller from '../controllers/images_controller'
 import {upload} from "../utils/multerStorage";
+import {IMAGES_ROUTE_URL_DELETE, IMAGES_ROUTE_URL_GET} from "../config/privateRoutes";
 
 const router = express.Router();
+
+router.get(IMAGES_ROUTE_URL_GET,  controller.getImages)
 
 //Upload new book image
 router.post("/upload", upload.single('cover'), controller.uploadImage)
 
 //Delete uploaded image file
-router.delete("/uploaded/delete/:id", controller.deleteUploadedImage)
+router.delete(IMAGES_ROUTE_URL_DELETE, controller.deleteUploadedImage)
 
 
 export default router;
