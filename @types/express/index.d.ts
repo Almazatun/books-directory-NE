0b572fcb1ae7f2e45d3127599567470934428388
Almaz <atunstyle@gmail.com>
2021-02-27 15:10:04 +0300

    :wrench: update file and delete custom express type * Deleted "files" filed and commented "typeRoot" filed also because in not need it anymore.

--- @types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import {IUploadFile} from "../../src/services/images_bll";
-
-declare global{
-    namespace Express {
-        export  interface Request {
-            uploadFile: IUploadFile
-        }
-    }
-}
