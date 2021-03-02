4b85ce38734ceb9279f9f137acf287557f4d9092
Almaz <atunstyle@gmail.com>
2021-03-02 13:10:32 +0300

    update methods and a little bit refactor * Updated UserDAL class methods (updateUserData, addBookUserCollection, deleteBookUserCollection, findBookUserCollection, getAllUsers).

--- src/dataAccessLayer/users.dal.ts
+++ src/dataAccessLayer/users.dal.ts
@@ -21,25 +21,85 @@ class Users {
         return createdUser
     }
 
-    async updateUserData (userId: string, userName: string) {
-        const updatedUserData = await User.findByIdAndUpdate(userId, {userName}, {new: true})
-            .populate('userBooks', ['title', 'publishDate', 'authorBook', 'imageBook'])
+    async updateUserData (userId: string, title: string) {
+
+        const updatedUserData = await User.findByIdAndUpdate(userId, {userName: title}, {new: true})
+            .populate(
+                {
+                    path: "userBooks",
+                    populate: {
+                        path: "imageBook", select: 'filePath',
+                    },
+                })
+            .populate({
+                path: "userBooks",
+                populate: {
+                    path: "authorBook", select: ['firstName', 'lastName'],
+                },
+            })
 
         return updatedUserData
     }
 
     async addBookUserCollection (userId: string, bookId: string) {
-        const updatedUserData = await User.findByIdAndUpdate(userId, {userBooks: [bookId]}, {new: true})
-            .populate('userBooks', ['title', 'publishDate', 'authorBook', 'imageBook'])
+
+        // @ts-ignore
+        //Have no idea how fix that the issue
+        //{_id: userId}  TS2322: Type 'string' is not assignable to type 'Condition '
+        const updatedUserData = await User.update({_id: userId},{
+
+            //$pull option
+            //https://docs.mongodb.com/manual/reference/operator/update/pull/
+            $push: {
+                userBooks: bookId
+            }
+        })
+            //multiply level populate
+            //https://mongoosejs.com/docs/populate.html#deep-populate
+            .populate(
+                {
+                    path: "userBooks",
+                    populate: {
+                        path: "imageBook", select: 'filePath',
+                    },
+                })
+            .populate({
+                path: "userBooks",
+                populate: {
+                    path: "authorBook", select: ['firstName', 'lastName'],
+                },
+            }).exec();
 
         return updatedUserData
     }
 
     async deleteBookUserCollection (userId: string, bookId: string) {
-        const updatedUserData = await User.findByIdAndUpdate(userId, {userBooks: [bookId].filter(bk => {
-            return bk !== bookId
-            })}, {new: true})
-            .populate('userBooks', ['title', 'publishDate', 'authorBook', 'imageBook'])
+
+        // @ts-ignore
+        //Have no idea how fix that the issue
+        //{_id: userId}  TS2322: Type 'string' is not assignable to type 'Condition '
+        const updatedUserData = await User.update({_id: userId},{
+
+            //$pull option
+            //https://docs.mongodb.com/manual/reference/operator/update/pull/
+            $pull: {
+                userBooks: bookId
+            }
+        })
+            .populate(
+                {
+                    path: "userBooks",
+                    populate: {
+                        path: "imageBook", select: 'filePath',
+                    },
+                })
+            .populate({
+                path: "userBooks",
+                populate: {
+                    path: "authorBook", select: ['firstName', 'lastName'],
+                },
+            }).exec();
+
 
         return updatedUserData
     }
@@ -52,11 +112,45 @@ class Users {
         return foundUser
     }
 
-    async findOneUserByUserName (userName: string) {
+    async findBookUserCollection (userId: string, bookId: string) {
 
-        const foundUser = await User.findOne({userName}).exec()
+        // @ts-ignore
+        const foundBookUserCollection = await User.find({_id: userId},
+            {
+            userBooks: bookId,
+        })
+
+        const foundBookId: object = foundBookUserCollection[0].userBooks
+
+        return foundBookId
+
+    }
+
+    async findOneUserById (userId: string) {
+
+        const foundUser = await User.findById(userId)
 
         return foundUser
+
+    }
+
+    async getAllUsers () {
+        const users = await User.find()
+            .populate(
+                {
+                    path: "userBooks",
+                    populate: {
+                        path: "imageBook", select: 'filePath',
+                    },
+                })
+            .populate({
+                path: "userBooks",
+                populate: {
+                    path: "authorBook", select: ['firstName', 'lastName'],
+                },
+            })
+
+        return users
     }
 
 }
