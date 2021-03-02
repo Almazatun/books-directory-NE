db33427588acebb5af68b2d33628eec7e590a0ec
Almaz <atunstyle@gmail.com>
2021-03-02 13:29:30 +0300

    :construction: include new options * In the App file added the users route and also included session option.

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,32 @@ import IndexRouter from "./routes/index";
 import Authors from "./routes/authors";
 import Books from "./routes/books";
 import Images from "./routes/images";
-import {DB_HOST} from "./config/database";
-import {CORS_ALLOW_HOST, CORS_WITH_CREDENTIALS} from "./config/cors";
-import {PORT} from "./config";
+import Users from './routes/users'
+import {DB_HOST} from "./configs/database";
+import {CORS_ALLOW_HOST, CORS_WITH_CREDENTIALS} from "./configs/cors";
+import {PORT} from "./configs";
+import session from "express-session";
+import mongoDBSession from 'connect-mongodb-session'
+import {MAX_AGE, SESSION} from "./configs/session";
+
+//Session db
+const DBSessions = mongoDBSession(session)
+
 
 //Create express app
 const app = express();
 app.use('/public' ,express.static('public'))
 
 //Config Object to Avoid Deprecation Warnings
-const config = { useNewUrlParser: true, useUnifiedTopology: true };
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+};
 
 //MongoDB
 //Connection database
-mongoose.connect(DB_HOST, config);
+mongoose.connect(DB_HOST, dbOptions);
 
 //Store Connection Object
 const db = mongoose.connection;
@@ -31,8 +43,28 @@ db.once("open", () => {
   console.log(err);
 });
 
+//Store session
+const sessionStore = new DBSessions({
+    uri: DB_HOST,
+    collection: 'sessions'
+})
+
+app.use(
+    session({
+        secret: SESSION,
+        resave: true,
+        saveUninitialized: false,
+        store: sessionStore,
+        cookie: {
+            sameSite: false,
+            maxAge: MAX_AGE,
+        }
+    })
+)
+
 //BodyParser
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 //Cors
 app.use(
@@ -47,6 +79,7 @@ app.use("/", IndexRouter);
 app.use("/authors", Authors);
 app.use("/books", Books);
 app.use("/images", Images);
+app.use("/users", Users);
 
 
 //Starting server
