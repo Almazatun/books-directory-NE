50f635191a81f49a3da69700f387bd97bfe9e284
Almaz <atunstyle@gmail.com>
2021-03-04 11:33:04 +0300

    :adhesive_bandage: simple fix

--- src/configs/session.ts
+++ src/configs/session.ts
@@ -1,5 +1,4 @@
-import {SESSION_SECRET} from "../confirm/sessionKey";
 import {v4} from 'uuid'
 
-export const SESSION = `${process.env.SESSION_KEY}_${v4()}` || SESSION_SECRET
+export const SESSION = `${process.env.SESSION_KEY}_${v4()}` || `${v4()}`
 export const MAX_AGE = 1000 * 60 * 60 * 3; // Three hours
