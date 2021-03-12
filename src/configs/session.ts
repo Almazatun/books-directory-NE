abb5cba7404c851c3a439974ff15cde2ca29932d
Almaz <atunstyle@gmail.com>
2021-03-12 15:49:59 +0300

    include process.env.MAX_AGE variable

--- src/configs/session.ts
+++ src/configs/session.ts
@@ -1,4 +1,4 @@
 import {v4} from 'uuid'
 
 export const SESSION = `${process.env.SESSION_KEY}_${v4()}` || `${v4()}`
-export const MAX_AGE = 1000 * 60 * 60 * 3; // Three hours
+export const MAX_AGE = Number(process.env.MAX_AGE) || 1000 * 60 * 60 * 3 // Three hours
