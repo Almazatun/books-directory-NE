acdb466fa3a90bfd732227608383e8cf10a8806b
Almaz <atunstyle@gmail.com>
2021-03-24 21:29:02 +0300

    update and include new env variable * DEV_MODE

--- src/configs/session.ts
+++ src/configs/session.ts
@@ -1,4 +1,5 @@
 import {v4} from 'uuid'
 
-export const SESSION = `${process.env.SESSION_KEY}_${v4()}` || `${v4()}`
-export const MAX_AGE = Number(process.env.MAX_AGE) || 1000 * 60 * 60 * 3 // Three hours
+export const DEV_MODE: string = process.env.DEV || "test";
+export const SESSION = DEV_MODE === "production" ? `${process.env.SESSION_KEY}_${v4()}` : `${v4()}`;
+export const MAX_AGE = Number(process.env.MAX_AGE) || 1000 * 60 * 60 * 3 // Three hours;
